14a517f48d41f60c0f8f219865f8ad90418156f7
maugrim <marshall@quander.me>
2015-11-18 17:24:01 -0800

    Redirect / to /spaces

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,6 +4,7 @@ var ReactDOM = require("react-dom");
 var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var IndexRedirect = ReactRouter.IndexRedirect;
 
 var Navigation = require("./navigation.jsx");
 var Spaces = require("./spaces.jsx");
@@ -29,6 +30,7 @@ var App = React.createClass({
 ReactDOM.render((
     <Router>
         <Route path="/" component={App}>
+            <IndexRedirect to="spaces"/>
             <Route path="spaces" component={Spaces}/>
             <Route path="spaces/new" component={Space}/>
             <Route path="space/:id" component={Space}/>
