16d690c4acca983acf150fcaa1568fc574838267
maugrim <marshall@quander.me>
2015-11-18 16:17:49 -0800

    Break up featured, non-featured spaces

--- src/js/spaces.jsx
+++ src/js/spaces.jsx
@@ -1,3 +1,4 @@
+var _ = require("lodash");
 var React = require("react");
 var ReactRouter = require("react-router");
 var Loading = require("./loading.jsx");
@@ -45,17 +46,33 @@ var Spaces = React.createClass({
         );
     },
 
-    render: function() {
-        if (this.state.spaces) {
+    renderSpaces: function(heading, spaces) {
+        if (spaces.length) {
             return (
                 <section>
-                    <h2>All spaces</h2>
-                    <div className="list-group">
-                        {this.state.spaces.map(this.renderSpace)}
-                    </div>
-                    <button onClick={this.onCreate} type="button" className="btn btn-primary">Create space</button>
+                    <h2>{heading}</h2>
+                    <div className="list-group">{spaces.map(this.renderSpace)}</div>
                 </section>
             );
+        }
+    },
+
+    render: function() {
+        if (this.state.spaces) {
+            if (this.state.spaces.length) {
+                var spaceLists = _.partition(this.state.spaces, "featured");
+                var featured = _.sortBy(spaceLists[0], "title");
+                var others = _.sortBy(spaceLists[1], "title");
+                return (
+                    <div>
+                        {this.renderSpaces("Featured spaces", featured)}
+                        {this.renderSpaces("Other spaces", others)}
+                        <button onClick={this.onCreate} type="button" className="btn btn-primary">Create space</button>
+                    </div>
+                );
+            } else {
+                return <p>There are no spaces yet. Perhaps you should <Link to="/spaces/new">create</Link> one?</p>
+            }
         } else {
             return <Loading />;
         }
