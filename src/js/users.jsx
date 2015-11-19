1bfa88701ac2cd1b2834c876e79ac08c41b54a72
maugrim <marshall@quander.me>
2015-11-18 18:36:51 -0800

    Cute Gravatar integration

--- src/js/users.jsx
+++ src/js/users.jsx
@@ -1,6 +1,7 @@
 var React = require("react");
 var ReactRouter = require("react-router");
 var Loading = require("./loading.jsx");
+var Gravatar = require("./gravatar.jsx");
 var Link = ReactRouter.Link;
 
 var data = require("./data.js");
@@ -25,10 +26,23 @@ var Users = React.createClass({
         });
     },
 
+    renderGravatar: function(user) {
+        if (user.email) {
+            return <Gravatar email={user.email} size={22}></Gravatar>
+        }
+    },
+
     renderUser: function(user, idx) {
         return (
             <Link className="list-group-item" to={"/user/" + user.id} key={idx}>
-                {user.first_name + " " + user.last_name}
+                <div className="media">
+                    <div className="media-left media-middle">
+                        {this.renderGravatar(user)}
+                    </div>
+                    <div className="media-body media-middle">
+                        {user.first_name + " " + user.last_name}
+                    </div>
+                </div>
             </Link>
         );
     },
